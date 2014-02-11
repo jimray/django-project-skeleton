72b53b4656f1de7ea3df697a7a8182d5e21cc8f2
Jim Ray <jimray@gmail.com>
2014-02-11 15:50:11 -0800

    Added a Procfile and cleaned up the 'import os'

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -8,7 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
 """
 
-import os
+# import os
+from os import environ
+from os.path import dirname, join
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -18,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Keep your secret key in an environment variable to help keep it from falling
+# into the wrong hands
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Default DEBUG to False for security reasons, ok to override in local
