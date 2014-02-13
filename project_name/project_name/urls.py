b19eb216f1f1e9c059b083af7ff72864994f5920
Jim Ray <jimray@gmail.com>
2014-02-13 13:04:17 -0800

    Put admin at the top of the default URLs

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+
     # include urls from your app like so
     # url(r'^', include('my_app.urls')),
-    url(r'^admin/', include(admin.site.urls)),
 )
