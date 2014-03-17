cf358860f3371e7bb0cf3065af13d90a91a5079b
Jim Ray <jimray@gmail.com>
2014-03-16 21:38:42 -0700

    Stop hard coding the DJANGO_SETTINGS_MODULE in manage.py
    Instead, set it as an .env var, with a tip on setting on Heroku

--- project_name/manage.py
+++ project_name/manage.py
@@ -3,7 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.local")
+    # Instead of setting the DJANGO_SETTINGS_MODULE here, we set it as an
+    # environment variable. Set in .env for local
+    # For heroku, set the variable like so
+    # heroku config:set DJANGO_SETTINGS_MODULE={{ project_name }}.settings.production
+    #
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.local")
 
     from django.core.management import execute_from_command_line
 
